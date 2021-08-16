003e99d4b923decf9fd73514f3de96be164f17db
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-08-16 13:40:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -262,8 +262,8 @@ def add_products():
             conn.commit()
             response["status_code"] = 201
             response['description'] = "Product added successfully"
-            return redirect('https://optimistic-benz-002fcf.netlify.app/admin.html')
-
+            # return redirect('https://optimistic-benz-002fcf.netlify.app/admin.html')
+            return response
 
 # a route to view all the products added
 @app.route('/api/show-products/', methods=["GET"])
